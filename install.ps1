0c6475fc8da89f0eb92ad91c20f77eda765be705
Mohammad Kebbi <66850675+moektel@users.noreply.github.com>
2023-03-20 06:57:24 -0700

    add dependencies

---
+++ install.ps1
@@ -0,0 +1,12 @@
+##### Run as Administrator #####
+
+# Powershell modules to install
+Install-Module Microsoft.PowerShell.SecretManagement -Repository PSGallery
+Install-Module Microsoft.PowerShell.SecretStore -Repository PSGallery
+
+# Install Chocolatey
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install Software packages
+choco install choco_software_packages.config --acceptlicense --confirm
